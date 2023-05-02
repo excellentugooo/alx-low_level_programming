8a5807cc73e7988bb4dd0b52bf23ff61f8cca9b0
excellentugooo <ugooo.okonkwo@gmail.com>
2023-05-02 11:33:51 -0700

    Puts

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 
 /**
- * puts2 - that prints every other character of a string, starting with the first character
+ * puts2 - prints every other character of a string
+ * starting with the first character
  * @str: string
  */
 
