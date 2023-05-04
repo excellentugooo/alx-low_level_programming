14f2270969052942924e5163627a250941ebdb78
excellentugooo <ugooo.okonkwo@gmail.com>
2023-05-04 07:59:59 -0700

    ‘Reverse’

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: array
+ * @n: number of elements of the array
+ *
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int x;
+	int y;
+
+	for (x = 0; x < n--; x++)
+	{
+		y = a[x];
+		a[x] = a[n];
+		a[n] = t;
+	}
+}
