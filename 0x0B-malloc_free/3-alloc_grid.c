24d18d6c0f7cdde88175ca944a6e366f03d6c73a
excellentugooo <ugooo.okonkwo@gmail.com>
2023-05-16 08:46:46 -0700

    Alloc_grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,4 +12,40 @@
 int **alloc_grid(int width, int height)
 {
 
+	int **ret;
+	int a, b;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	ret = malloc(height * sizeof(int));
+
+	if (ref == NULL)
+	{
+		return (NULL);
+	}
+	for (a = 0; a < height; a++)
+	{
+		ret[a] = malloc(width * sizeof(int));
+
+		if (ret[a] == NULL)
+		{
+			for (; a >= 0; a--)
+			{
+				free(ret[a]);
+			}
+			free(ret);
+			return (NULL);
+		}
+	}
+	for (a = 0; a < height; a++)
+	{
+		for (b = 0; b < width; b++)
+		{
+			ret[a][b] = 0;
+		}
+	}
+	return (mee);
 }
