ec33abab8e9c579c6a87c0a66298205d2b4e8086
excellentugooo <ugooo.okonkwo@gmail.com>
2023-05-16 10:07:23 -0700

    Argstostr

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenates all the arguments of your program.
+ * @ac: int
+ * @av: double pointer array
+ *
+ * Return: 0
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int a, b, c = 0, d = 0;
+	char *s;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	for (a = 0; a < ac; a++)
+	{
+		for (b = 0; av[a][b]; b++)
+		{
+			d++;
+		}
+	}
+	d += ac;
+
+	s = malloc(sizeof(char) * d + 1);
+
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	for (a = 0; a < ac; a++)
+	{
+		for (b = 0; av[a][b]; b++)
+		{
+			s[c] = av[a][b];
+			c++;
+		}
+	}
+	if (s[c] == '\0')
+	{
+		s[c++] = '\n';
+	}
+	return (s);
+}
