4c6cc4af8407b3841b03b0eee145babd6bf4c7ff
excellentugooo <ugooo.okonkwo@gmail.com>
2023-05-16 06:59:46 -0700

    Concatenate

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,5 +12,37 @@
 char *str_concat(char *s1, char *s2)
 {
 
+	char *cat;
+	int a, b;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	cat = malloc((a + b + 1) * sizeof(char));
+
+	if (cat == NULL)
+	{
+		return (NULL);
+	}
+	a = 0;
+	while (s1[a] != '\0')
+	{
+		cat[a] = s1[a];
+		a++;
+	}
+	b = 0;
+	while (s2[b] != '\0')
+	{
+		cat[a] = s1[b];
+		a++, b++;
+	}
+	cat = '\0';
+
+	return (cat);
 }
