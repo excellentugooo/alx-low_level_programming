50cf2b0673a2df28dc18c68c27fdb093ff5b2f29
excellentugooo <ugooo.okonkwo@gmail.com>
2023-05-18 13:20:43 -0700

    Multiply

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -5,10 +5,10 @@
 #define ERR_MSG "Error"
 
 /**
- * _isdigit - check if numbers are 0 - 9
+ * _isdigit - checks if a string contains a non-digit char
  * @c: string
  *
- * Return: 0 or 1
+ * Return: 0 if a non-digit,else 1
  */
 
 int _isdigit(char *c)
@@ -18,29 +18,30 @@ int _isdigit(char *c)
 	while (c[x])
 	{
 		if (c[x] < '0' || c[x] > '9')
+		{
 			return (0);
+		}
 		x++;
 	}
-	return (1);	
+	return (1);
 }
 
 /**
- * _strlen - a function that returns the length of a string.
- * @s: string
+ * _strlen - returns the length of a string
+ * @c: string
  *
- * Return: lenght
+ * Return: the length of the string
  */
 
-int _strlen(char *s)
+int _strlen(char *c)
 {
 	int lenght = 0;
 
-	while (*s != '\0')
+	while (c[lenght] != '\0')
 	{
-	lenght++;
-	s++;
+		lenght++;
 	}
-return (lenght);
+	return (lenght);
 }
 
 /**
@@ -49,14 +50,13 @@ return (lenght);
 
 void errors(void)
 {
-
 	printf("Error\n");
 	exit(98);
 }
 
 /**
- * main - multiplies two positive numbers
- * @argc: number of arguments
+ * main - multiplies two positive numb
+ * @argc: number of argument
  * @argv: array of arguments
  *
  * Return: always 0 (Success)
@@ -65,43 +65,39 @@ void errors(void)
 int main(int argc, char *argv[])
 {
 	char *str1, *str2;
-	int l1, l2, lenght, x, c;
-	int d1, d2, *result, y = 0;
+	int lenght1, lenght2, lenght, x, c, d1, d2, *result, y = 0;
 
 	str1 = argv[1], str2 = argv[2];
 	if (argc != 3 || !_isdigit(str1) || !_isdigit(str2))
-	{
 		errors();
-	}
-	l1 = _strlen(str1);
-	l2 = _strlen(str2);
-	lenght = l1 + l2 + 1;
-
+	lenght1 = _strlen(str1);
+	lenght2 = _strlen(str2);
+	lenght = lenght1 + lenght2 + 1;
 	result = malloc(sizeof(int) * lenght);
 	if (!result)
 		return (1);
-	for (x = 0; x <= l1 + l2; x++)
+	for (x = 0; x <= lenght1 + lenght2; x++)
 		result[x] = 0;
-	for (l1 = l1 - 1; l1 >= 0; l1--)
+	for (lenght1 = lenght1 - 1; lenght1 >= 0; lenght1--)
 	{
-		d1 = str1[l1] - '0';
+		d1 = str1[lenght1] - '0';
 		c = 0;
-		for (l2 = _strlen(str2) - 1; l2 >= 0; l2--)
+		for (lenght2 = _strlen(str2) - 1; lenght2 >= 0; lenght2--)
 		{
-			d2 = str2[l2] - '0';
-			c += result[l1 + l2 + 1] + (d1 * d2);
-			result[l1 + l2 + 1] = c % 10;
+			d2 = s2[lenght2] - '0';
+			c += result[lenght1 + lenght2 + 1] + (d1 * d2);
+			result[lenght1 + lenght2 + 1] = c % 10;
 			c /= 10;
 		}
 		if (c > 0)
-			result[l1 + l2 + 1] += c;
+			result[lenght1 + lenght2 + 1] += c;
 	}
 	for (x = 0; x < lenght - 1; x++)
 	{
 		if (result[x])
 			y = 1;
 		if (y)
-		_putchar(result[x] + '0');
+			_putchar(result[x] + '0');
 	}
 	if (!y)
 		_putchar('0');
