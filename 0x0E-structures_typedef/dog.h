22ca7b8cfbe00b60f16e264da06167501227e19a
excellentugooo <ugooo.okonkwo@gmail.com>
2023-05-22 10:12:14 -0700

    Dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dogs data
+ * @name: First member
+ * @age: second member
+ * @owner: third owner
+ *
+ * Description: this contains the basic info of a dogs
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
