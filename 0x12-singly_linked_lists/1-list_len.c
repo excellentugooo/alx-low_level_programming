00d349a181de4627d1364d83ae79d10359c5f95c
excellentugooo <ugooo.okonkwo@gmail.com>
2023-06-01 07:33:43 -0700

    Node lenght

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements
+ * in a linked list_t list.
+ * @h: pointer to element
+ *
+ * Return: s
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t s;
+
+	for (s = 1; h != NULL; s++)
+	{
+		h = h->next;
+	}
+	return (s);
+}
