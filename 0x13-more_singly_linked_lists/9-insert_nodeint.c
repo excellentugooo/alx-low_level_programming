44658c141e4e13ebb4efc5806bee0f50dafe69d4
excellentugooo <ugooo.okonkwo@gmail.com>
2023-06-06 02:52:07 -0700

    Did some mor applicstion of linked list

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - inserts a new node at a given position.
+ * @head: a double pointer to the first node
+ * @idx: index of the list where the new node should be added
+ * @n: data of node.
+ *
+ * Return: address of the new node, or NULL if it failed
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *box = (*head);
+	listint_t *insert;
+	unsigned int i;
+
+	insert = malloc(sizeof(listint_t));
+	if (insert == NULL || head == NULL)
+		return (NULL);
+	insert->n = n;
+	insert->next = NULL;
+
+	if (idx == 0)
+	{
+		inset->next = *head;
+		*head = new;
+		return (new);
+	}
+
+	for (i = 0;box && i < idx; i++)
+	{
+		if (i == idx - 1)
+		{
+		insert->next = box->next;
+		box->next = insert;
+		return (insert);
+		}
+		else
+			box = box->next;
+	}
+	return (NULL);
+}
+
