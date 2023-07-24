2a766f165ae731884ff350ebadb7f2fd8e8eaccc
excellentugooo <ugooo.okonkwo@gmail.com>
2023-07-24 04:41:58 -0700

    advanced debug codes

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-wget -P /tmp https://github.com/excellentugooo/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD=/tmp/nrandom.so
+echo -e '#include <stdio.h>\n#include <stdlib.h>\n\nvoid print_winning_message() { printf("--> Please make me win!\\n"); }\n\nint rand() { return 6; }\n\nint rand_r(unsigned int *seedp) { return 6; }\n\nlong int random() { return 6; }' > /tmp/hack.c && gcc -Wall -shared -fPIC -o /tmp/nrandom.so /tmp/hack.c
